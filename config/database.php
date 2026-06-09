460a7a4f6734e466de7972857334ebca226f030a
GabrielSonvezzo <sonvezzogabriel@gmail.com>
2026-06-09 20:01:38 -0300

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -1,30 +1,33 @@
 <?php
 
 class Database {
+    private static $instance = null;
+    private $conn;
 
-    private $host = "localhost";
+    private $host   = "localhost";
     private $dbname = "marketplace";
-    private $user = "root";
-    private $pass = "";
+    private $user   = "root";
+    private $pass   = "positivo";
 
-    public $conn;
-
-    public function conectar() {
-
+    private function __construct() {
         try {
             $this->conn = new PDO(
                 "mysql:host={$this->host};dbname={$this->dbname};charset=utf8",
                 $this->user,
                 $this->pass
             );
-
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            return $this->conn;
 
         } catch (PDOException $e) {
             die("Erro na conexão: " . $e->getMessage());
         }
+    }
+
+    public static function getConnection() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance->conn;
     }
 }
